b372b1140dfff1e74ee5c86cb35480878d8c8418
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-09-07 10:39:15 +0100

    updated

--- create_cloudy_input_grid.sh
+++ create_cloudy_input_grid.sh
@@ -4,7 +4,7 @@ $CLOUDY_PATH=$CLOUDY_PATH
 
 while read GRID_NAME; do
   echo "$GRID_NAME"
-  python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy_path $CLOUDY_PATH
+  python ../create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy_path $CLOUDY_PATH
 done < grids.txt
 
 
