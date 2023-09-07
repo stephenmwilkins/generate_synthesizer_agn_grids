c04dc785de22f242cc4d762c0fc2f602dd9b7644
Stephen Wilkins <s.wilkins@sussex.ac.uk>
2023-09-07 12:19:37 +0100

    updated

--- create_cloudy_input_grid.sh
+++ create_cloudy_input_grid.sh
@@ -3,8 +3,9 @@ SYNTHESIZER_DATA_DIR="/research/astrodata/highz/synthesizer/"
 $CLOUDY_PATH=$CLOUDY_PATH
 
 while read GRID_NAME; do
-  echo "$GRID_NAME"
-  python ../create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy_path $CLOUDY_PATH
+  cd ..
+  python create_cloudy_input_grid.py -machine $MACHINE -synthesizer_data_dir $SYNTHESIZER_DATA_DIR -grid_name $GRID_NAME -cloudy_path $CLOUDY_PATH
+  cd generate_synthesizer_agn_grids
 done < grids.txt
 
 
